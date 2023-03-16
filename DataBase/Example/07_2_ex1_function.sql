6502921d11c932578b47af15f8a70ab76dfd7997
ldh1103 <ldh517525@gmail.com>
2023-03-16 16:52:39 +0900

    23-03-16

--- DataBase/Example/07_2_ex1_function.sql
+++ DataBase/Example/07_2_ex1_function.sql
@@ -3,10 +3,22 @@
 -- 	CAST( 값 AS 데이터형식 )
 -- 	CONVERT( 값, 데이터형식 )
 
+SELECT CAST(1234 AS CHAR(4));
+SELECT CONVERT(1234, CHAR(4));
+
 -- 2. 제어 흐름 함수
 -- 	IF(수식, 참일 경우 결과, 거짓일 경우 결과) : 수식이 참 또는 거짓에 따라 결과를 분기합니다.
+SELECT IF( 1 < 2, '참', '거짓' );
+
+SELECT emp_no, IF( emp_no = 10001 , first_name, birth_date )
+FROM employees;
+
 -- 	IFNULL(수식1, 수식2) : 수식이 NULL일 경우 수식2를, NULL이 아닐 경우 수식1을 반환합니다.
+SELECT IFNULL(' ', 'aa');
+
 -- 	NULLIF(수식1, 수식2) : 수식1과 2가 같으면 NULL을 반환하고, 다르면 수식1을 반환합니다.
+SELECT NULLIF(1 , 1);
+
 -- 	CASE ~ WHEN ~ ELSE ~ END : 다중 분기를 위해 사용합니다.
 -- 		예)
 -- 			CASE 체크하려는 수식1
@@ -15,38 +27,119 @@
 -- 				WHEN 분기수식3 THEN 결과3
 -- 				ELSE 결과4
 -- 			END
+SELECT 
+	emp_no
+	,gender
+	,CASE gender
+		WHEN 'M' THEN '남자'
+--		WHEN 'F' THEN '여자'
+		ELSE '여자'
+	END
+FROM employees LIMIT 10;
+
+SELECT 
+	emp_no
+	,CASE title 
+			WHEN 'Senior Engineer' THEN '관리자'
+			ELSE '팀원'
+		END AS 'K_title'
+FROM titles;
 	
 -- 3. 문자열 함수
 -- 	CONCAT(문자열1, 문자열2, ...) : 문자열을 이어줍니다.
+SELECT CONCAT('안녕하세요', ' ','좋은아침입니다');
+SELECT CONCAT(last_name, ' ', first_name) AS fullname
+FROM employees
+WHERE emp_no = 500000;					
+
 -- 	CONCAT_WS(구분자, 문자열1, 문자열2, ...) : 문자열 사이에 구분자를 넣어 이어줍니다.
+SELECT CONCAT_WS(' / ', 'a', 'b', 'c');
+SELECT CONCAT_WS(' / ',last_name, first_name) AS fullname
+FROM employees
+WHERE emp_no = 500000;	
+
 -- 	FORMAT(숫자, 소숫점 자릿수) : 숫자에 ','를 넣어주고, 소수점 자릿수 까지 표현합니다.
+SELECT FORMAT(123456, 2);
+
 -- 	LEFT(문자열, 길이) : 문자열을 왼쪽부터 길이만큼 잘라 반환합니다.
+SELECT LEFT('abcdefg', 3);
+
 -- 	RIGHT(문자열, 길이) : 문자열을 오른쪽부터 길이만큼 잘라 반환합니다.
+SELECT RIGHT('abcdefg', 3);
+
 -- 	UPPER(문자열) : 소문자를 대문자로 변경합니다.
+SELECT UPPER('abcdEFG');
+
 -- 	LOWER(문자열) : 대문자를 소문자로 변경합니다.
+SELECT LOWER('ABCDefg');
+
 -- 	LPAD(문자열, 길이, 채울 문자열) : 문자열을 포함해 길이만큼 채울 문자열을 왼쪽에 넣습니다.
+SELECT LPAD('abc', 5, '@');
+SELECT LPAD('956', 6, 0);
+
 -- 	RPAD(문자열, 길이, 채울 문자열) : 문자열을 포함해 길이만큼 채울 문자열을 오른쪽에 넣습니다.
+SELECT RPAD('abc', 5, '@');
+SELECT RPAD('956', 6, 0);
+
 -- 	TRIM(문자열) : 좌우 공백을 제거합니다.
+SELECT TRIM('            a bc                ');
+
 -- 	LTRIM(문자열) : 왼쪽 공백을 제거합니다.
+SELECT LTRIM('           a bc                ');
+
+
 -- 	RTRIM(문자열) : 오른쪽 공백을 제거합니다.
+SELECT RTRIM('           a bc                ');
+
+
 -- 	TRIM(방향 문자열1 FROM 문자열2) : 방향을 지정해 문자열2에서 문자열1을 제거합니다.
 -- 		** 방향을 LEADING(좌), BOTH(좌우), TRAILING(우) **
+SELECT TRIM(LEADING 'abc' FROM 'abcdef');
+SELECT TRIM(TRAILING 'abc' FROM 'abcdefabc');
+SELECT TRIM(BOTH 'abc' FROM 'abcdefabc');
+
 -- 	SUBSTRING(문자열, 시작위치, 길이) : 문자열을 시작위치에서 길이만큼 잘라서 반환합니다.
--- 	SUBSTRING(문자열, 구분자, 횟수) : 왼쪽부터 구분자가 횟수 번째가 나오면 그 이후부터 버립니다.
+SELECT SUBSTRING('abcdef', 2, 4); -- 2~4까지 출력하고, 나머진 자름
+
+-- 	SUBSTRING_INDEX(문자열, 구분자, 횟수) : 왼쪽부터 구분자가 횟수 번째가 나오면 그 이후부터 버립니다.
+SELECT SUBSTRING_INDEX('ab.cd.ef.gh', '.', 3); -- 3번째 .이 나오면 그부분부터 자름
 
 -- 4. 수학 함수
 -- 	CEILING(숫자) : 올림합니다.
+SELECT CEILING(1.1);
+
 -- 	FLOOR(숫자) : 내림합니다.
+SELECT FLOOR(1.1);
+
 -- 	ROUND(숫자) : 반올림합니다.
+SELECT ROUND(1.5);
+
 -- 	TRUNCATE(숫자, 정수) : 소수점 기준으로 정수위치 까지 구하고 나머지는 버립니다.
+SELECT TRUNCATE(1.11, 1);
 
 -- 5. 날짜 및 시간 함수
 -- 	NOW() : 현재 날짜/시간을 구합니다. (YYYY-MM-DD HH:MM:DD)
+SELECT NOW();
+SELECT DATE(20230316);
+
 -- 	ADDDATE(날짜1, INTERVAL 날짜2) : 날짜1에서 날짜2를 더한 날짜를 구합니다.
--- 	SUBDATE(날짜1, INTERVAL 날짜2) : 날짜1에서 날짜2를 뺍 날짜를 구합니다.
+SELECT ADDDATE(NOW(), INTERVAL -1 DAY); -- day , month, etc...
+
+-- 	SUBDATE(날짜1, INTERVAL 날짜2) : 날짜1에서 날짜2를 뺀 날짜를 구합니다.
+SELECT SUBDATE(NOW(), INTERVAL 1 DAY); 
+
 -- 	ADDTIME(날짜/시간, 시간) : 날짜/시간에서 시간를 더한 날짜/시간를 구합니다.
--- 	SUBTIME(날짜/시간, 시간) : 날짜/시간에서 시간를 뺍 날짜/시간를 구합니다.
+SELECT ADDTIME(NOW(), '1:1:1'); -- 1시간 1분 1초를 더한다
+
+-- 	SUBTIME(날짜/시간, 시간) : 날짜/시간에서 시간를 뺀 날짜/시간를 구합니다.
+SELECT SUBTIME(NOW(), '1:1:1');
 
 -- 6. 순위 함수
 -- 	RANK() OVER(ORDER BY 속성명 DESC/ASC) : 순위를 매깁니다.
+SELECT emp_no, RANK() OVER(ORDER BY salary ASC) r, salary
+FROM salaries;
+
 -- 	ROW_NUMBER() OVER(ORDER BY 속성명 DESC/ASC) : 레코드에 순위를 매깁니다.
+SELECT emp_no, ROW_NUMBER() OVER(ORDER BY salary ASC) r, salary
+FROM salaries;
+
---
+++ DataBase/Training/01_3_tng_join.sql
@@ -0,0 +1,126 @@
+-- 사원의 사원번호, 풀네임, 직책명을 출력
+SELECT 
+	emp.emp_no
+	, CONCAT(emp.last_name, ' ', emp.first_name) AS fullname
+	, ti.title
+FROM employees emp
+	INNER JOIN titles ti
+		ON emp.emp_no = ti. emp_no
+;
+
+-- 사원의 사원번호, 성별, 현재 월급을 출력
+SELECT 
+	emp.emp_no
+	, emp.gender
+	, sal.salary
+FROM employees emp
+	INNER JOIN salaries sal
+		ON emp.emp_no = sal.emp_no
+WHERE sal.to_date >= NOW()
+;
+
+-- 10010사원의 풀네임, 과거부터 현재까지 월급 이력을 출력
+SELECT 
+	CONCAT(emp.last_name, ' ', emp.first_name) AS fullname
+	, sal.salary
+FROM employees emp
+	INNER JOIN salaries sal
+		ON emp.emp_no = sal.emp_no
+WHERE emp.emp_no = 10010
+;
+
+-- 사원의 사원번호, 풀네임, 소속부서명을 출력
+SELECT 
+	emp.emp_no
+	, CONCAT(emp.last_name, ' ', emp.first_name) AS fullname
+	, dep.dept_name
+FROM employees emp
+	INNER JOIN dept_emp dept
+		ON emp.emp_no = dept.emp_no
+	INNER JOIN departments dep
+		ON dept.dept_no = dep.dept_no
+WHERE dept.to_date >= NOW()
+ORDER BY emp.emp_no
+;
+
+
+-- 현재 월급의 상위 10위까지 사원의 사번, 풀네임, 월급을 출력
+SELECT 
+	emp.emp_no
+	, CONCAT(emp.last_name, ' ', emp.first_name) AS fullname
+	, sal.salary
+FROM employees emp
+	INNER JOIN salaries sal
+		ON emp.emp_no = sal.emp_no
+WHERE sal.to_date >= NOW()
+ORDER BY sal.salary DESC
+LIMIT 10
+;
+
+-- 각 부서의 부서장의 부서명, 풀네임, 입사일을 출력
+SELECT 
+	dep.dept_name
+	, CONCAT(emp.last_name, ' ', emp.first_name) AS fullname
+	, emp.hire_date
+FROM employees emp
+	INNER JOIN dept_manager dept
+		ON emp.emp_no = dept.emp_no
+	INNER JOIN departments dep
+		ON dept.dept_no = dep.dept_no
+WHERE dept.to_date >= NOW();
+
+-- 현재 직책이 "Staff"인 사원의 현재 평균 월급을 출력
+SELECT  AVG(sal.salary)
+FROM titles ti
+	INNER JOIN salaries sal
+		ON sal.emp_no = ti.emp_no
+WHERE ti.title = 'staff'
+	AND sal.to_date >= NOW()
+	AND ti.to_date >= NOW();
+
+-- 부서장직을 역임했던 모든 사원의 풀네임과 입사일 사번 부서번호를 출력
+SELECT 
+	CONCAT(emp.last_name, ' ', emp.first_name) AS fullname
+	, emp.hire_date
+	, emp.emp_no
+	, dept.dept_no
+FROM employees emp
+	INNER JOIN dept_manager dept
+		ON emp.emp_no = dept.emp_no
+;
+
+-- 현재 각 직급별 평균월급 중 60,000이상인 직급의 직급명, 평균월급(정수)를 내림차순으로 출력
+SELECT 
+	ti.title
+	, TRUNCATE(AVG(sal.salary), 0) AS avg_sal
+FROM titles ti
+	INNER JOIN salaries sal
+		ON ti.emp_no = sal.emp_no
+WHERE sal.to_date >= NOW()
+	AND ti.to_date >= NOW()
+GROUP BY ti.title
+HAVING avg_sal >= 60000
+ORDER BY avg_sal DESC;
+
+-- 성별이 여자인 사원들의 직급별 사원수를 출력
+SELECT 
+	ti.title
+	, COUNT(*) AS '사원 수'
+FROM employees emp
+	INNER JOIN titles ti
+		ON emp.emp_no = ti.emp_no
+WHERE emp.gender = 'F'
+	AND ti.to_date >= NOW()
+GROUP BY ti.title;
+
+-- 직급별 퇴사한 남자 사원수
+SELECT A.gender, COUNT(A.gender) AS cnt
+FROM employees A
+INNER JOIN (
+	SELECT emp_no
+	FROM titles
+	GROUP BY emp_no
+	HAVING MAX(to_date) != DATE(99990101)
+) B
+ON A.emp_no = B.emp_no
+GROUP BY A.gender;
