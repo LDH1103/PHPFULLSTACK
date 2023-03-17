b9a380b94f9a0f27c2186f83ca3ca95c746b7f04
ldh1103 <ldh517525@gmail.com>
2023-03-17 17:43:12 +0900

    23-03-17

--- DataBase/Training/08_2_tng_view.sql
+++ DataBase/Training/08_2_tng_view.sql
@@ -1,9 +1,9 @@
 CREATE OR REPLACE VIEW test_view
 AS 
 	SELECT 
-		emp.emp_no
-		, CONCAT(emp.last_name, ' ', emp.first_name) AS fullname
-		, dep.dept_name
+		emp.emp_no '사원 번호'
+		, CONCAT(emp.last_name, ' ', emp.first_name) fullname
+		, dep.dept_name '소속 부서 명'
 	FROM employees emp
 		INNER JOIN dept_emp dept
 			ON emp.emp_no = dept.emp_no
