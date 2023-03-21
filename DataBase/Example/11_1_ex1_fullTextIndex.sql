e78cd70b8cf2483dda0e3a90164526ecddca707c
ldh1103 <ldh517525@gmail.com>
2023-03-21 15:39:53 +0900

    23-03-21

--- DataBase/Example/11_1_ex1_fullTextIndex.sql
+++ DataBase/Example/11_1_ex1_fullTextIndex.sql
@@ -7,6 +7,20 @@
 		, FULLTEXT 인덱스명(컬럼) 
 	)
 
+	CREATE TABLE test_text (
+	txt_no INT PRIMARY KEY AUTO_INCREMENT
+	, f_text VARCHAR(4000)
+	, FULLTEXT idx_full_test_text_f_text(f_text)
+)
+;
+
+INSERT INTO test_text(f_text) VALUES('동해물과 백두산이 마르고 닳고록 하느님이 보우하사 우리나라만세');
+INSERT INTO test_text(f_text) VALUES('무궁화 삼천리 화려강산 대한사람 대한으로 길이 보전하세');
+INSERT INTO test_text(f_text) VALUES('남산위에 저소나무 철갑을 두른듯 바람서리 불변함은 우리기상일세');
+INSERT INTO test_text(f_text) VALUES('가을 하늘 공활한데 높고 구름 없이 밝은 달은 우리 가슴 일편단심일세');
+INSERT INTO test_text(f_text) VALUES('이 기상과 이 맘으로 충성을 다하여 괴로우나 즐거우나 나라 사랑하세');
+
+
 	CREATE FULLTEXT INDEX 인덱스명 ON 테이블(컬럼);
 
 3. 전체 텍스트 인덱스 삭제
@@ -16,6 +30,9 @@
 	-- 검색어1 또는 검색어2 (or 검색)
 	SELECT * FROM Table_Name
 	WHERE MATCH(컬럼) AGAINST('검색어1 검색어2');
+	
+	SELECT * FROM test_text
+	WHERE MATCH(f_text) AGAINST('동해물과');
 	
 	-- 검색어 중간에 공백이 들어가는 경우
 	SELECT * FROM Table_Name
