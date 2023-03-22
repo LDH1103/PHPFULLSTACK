5876d4941b6870ea21c8e6282e6673db6ec6d279
ldh1103 <ldh517525@gmail.com>
2023-03-22 18:10:42 +0900

    23-03-22

--- PHP/Training/04_1_tng1_switch.php
+++ PHP/Training/04_1_tng1_switch.php
@@ -1,30 +1,33 @@
 <?php
 
-$score = 100;
+$score = 0;
 $y_score = '당신의 점수는';
 $scroe_is = '점 입니다';
 $blank = ' ';
 $grade = '<A>';
 
 switch ($score) {
-    case $score > 100 || $score <= 0 :
+    case $score > 100 || $score < 0 :
         echo '잘못된 값을 입력 했습니다.';
         break;
     case $score === 100 :
         $grade = '<A+>';
         echo $y_score.$blank.$score.$scroe_is.$blank.$grade;
         break;
-    case $score >= 90 :
+    case $score === 90 :
         $grade = '<A>';
         echo $y_score.$blank.$score.$scroe_is.$blank.$grade;
         break;
-    case $score >= 80 :
+    case $score === 80 :
+        $grade = '<B>';
         echo $y_score.$blank.$score.$scroe_is.$blank.$grade;
         break;
-    case $score >= 70 :
+    case $score === 70 :
+        $grade = '<C>';
         echo $y_score.$blank.$score.$scroe_is.$blank.$grade;
         break;
-    case $score >= 60 :
+    case $score === 60 :
+        $grade = '<D>';
         echo $y_score.$blank.$score.$scroe_is.$blank.$grade;
         break;
     
