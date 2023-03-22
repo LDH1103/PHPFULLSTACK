b5987237affbf413d5d514db4b04425f0f13479a
ldh1103 <ldh517525@gmail.com>
2023-03-22 13:19:58 +0900

    23-03-22

---
+++ PHP/Training/03_1_tng1_basic.php
@@ -0,0 +1,30 @@
+<?php
+
+    echo "점심메뉴\n";
+    echo "탕수육 8,000\n";
+    echo "짜장면 6,000\n";
+    echo "짬뽕 6,000\n\n";
+
+    echo "점심메뉴\n탕수육 8,000\n짜장면 6,000\n짬뽕 6,000\n\n";
+
+    $tang = "탕수육";
+    $zza = "짜장면";
+    $zzam = "짬뽕";
+    $blank = " ";
+    $line = "\n";
+    $price_8000 = "8,000";
+    $price_6000 = "6,000";
+
+    echo "점심메뉴$line";
+    echo $tang.$blank.$price_8000.$line;
+    echo $zza.$blank.$price_6000.$line;
+    echo $zzam.$blank.$price_6000.$line.$line;
+
+    $p6000 = 6000;
+    $p2000 = 2000;
+
+    echo "점심메뉴$line";
+    echo $tang.$blank.$p6000+$p2000.$line;
+    echo $zza.$blank.$p6000.$line;
+    echo $zzam.$blank.$p6000;
+?>
---
+++ PHP/Training/03_2_tng1_operator.php
@@ -0,0 +1,44 @@
+<?php
+
+    //연산자를 이용해 계산
+    $tng_num = 10;
+    echo "산술 연산자\n";
+    
+    // 10을 더하기
+    echo $tng_num + 10, "\n";
+
+    //5로 나누기
+    echo $tng_num / 5, "\n";
+
+    //4를 뺴기
+    echo $tng_num - 4, "\n";
+
+    //7로 나눈 나머지를 구하기
+    echo $tng_num % 7, "\n";
+
+    //3을 곱하기
+    echo $tng_num * 3, "\n";
+    echo "\n\n";
+
+
+
+
+    //산술 대입 연산자를 이용해 계산
+    $tng_num = 10;
+    echo "산술 대입 연산자\n"; 
+
+    // 10을 더하기
+    echo $tng_num += 10, "\n"; //tng_num = tng_num + 10 이기때문에, tng_num값이 20으로 갱신됨
+
+    //5로 나누기
+    echo $tng_num /= 5, "\n"; //tng_num = tng_num / 5 이기때문에, tng_num값이 4로 갱신됨
+
+    //4를 빼기
+    echo $tng_num -= 4, "\n";
+
+    //7로나눈 나머지를 구하기
+    echo $tng_num %= 7, "\n";
+
+    //3을 곱하기    
+    echo $tng_num *= 3, "\n";
+?>
