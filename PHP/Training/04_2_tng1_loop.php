ba2576453fec576d299ec8de4026b54d9b2cd2cd
LDH1103 <ldh517525@gmail.com>
2023-03-23 22:05:39 +0900

    23-03-23

--- PHP/Training/04_2_tng1_loop.php
+++ PHP/Training/04_2_tng1_loop.php
@@ -50,41 +50,57 @@
     // }
 
     // 피라미드
-    $max_no = 15;
-    for ($n_1 = 0; $n_1 < $max_no; $n_1++)
-    {
-    for ($n_2 = $max_no - $n_1; $n_2 > 0; $n_2--)
-    {
-        echo " ";
-    }
-    for ($n_3 = 0; $n_3 < $n_1; $n_3++)
-    {
-        echo "*";
-    }
-    for ($n_4 = 1; $n_4 < $n_1; $n_4++)
-    {
-        echo "*";
-    }
-    echo "\n";
-    }
+    // $max_no = 15;
+    // for ($n_1 = 0; $n_1 < $max_no; $n_1++)
+    // {
+    // for ($n_2 = $max_no - $n_1; $n_2 > 0; $n_2--)
+    // {
+    //     echo " ";
+    // }
+    // for ($n_3 = 0; $n_3 < $n_1; $n_3++)
+    // {
+    //     echo "*";
+    // }
+    // for ($n_4 = 1; $n_4 < $n_1; $n_4++)
+    // {
+    //     echo "*";
+    // }
+    // echo "\n";
+    // }
     
     
     // 마름모
-    $max_no = 15;
-    for ($n_1 = 0; $n_1 < $max_no; $n_1++)
-    {
-        for ($n_2 = $max_no - $n_1; $n_2 > 0; $n_2--)
-        {
+    $max = 10;
+    for ($n1 = 0; $n1 <= $max; $n1++) 
+    { 
+        for ($blank = $max - $n1; $blank > 0; $blank--) 
+        { 
             echo " ";
         }
-        for ($n_3 = 0; $n_3 < $n_1; $n_3++)
-        {
+        for ($n2 = 0; $n2 <= $n1; $n2++) 
+        { 
             echo "*";
         }
-        for ($n_4 = 1; $n_4 < $n_1; $n_4++)
-        {
+        for ($n3 = 1; $n3 <= $n1; $n3++) 
+        { 
             echo "*";
         }
-    echo "\n";
+        echo "\n";
+    }
+    for ($n1 = 0; $n1 <= $max; $n1++) 
+    { 
+        for ($blank = 0; $blank < $n1; $blank++) 
+        { 
+            echo " ";
+        }
+        for ($n2 = 1; $n2 <= $max - $n1; $n2++) 
+        { 
+            echo "*";
+        }
+        for ($n3 = 1; $n3 <= $max - $n1; $n3++) 
+        { 
+            echo "*";
+        }
+        echo "\n";
     }
 ?>
---
+++ PHP/Training/04_2_tng_loop_self.php
@@ -0,0 +1,184 @@
+<?php
+
+    // 직각삼각형
+
+    // $max = 10;
+
+    // for ($n1 = 0; $n1 <= $max; $n1++) 
+    // {
+    //     for ($n2 = 0; $n2 < $n1 ; $n2++) { 
+    //         echo "*";
+    //     }
+    //     echo "*\n";
+    // }
+
+
+
+    // 직각삼각형 반대로 붙이기
+
+    // $max = 10;
+
+    // for ($n1 = 0; $n1 <= $max; $n1++) 
+    // {
+    //     for ($blank = $max - $n1; $blank > 0 ; $blank--) { 
+    //         echo " ";
+    //     }
+    //     for ($n2 = 0; $n2 < $n1 ; $n2++) { 
+    //         echo "*";
+    //     }
+    //     echo "\n";
+    // }
+
+    // $max = 10;
+
+    // for ($n1 = 0; $n1 <= $max; $n1++) {
+    //     for ($blank = $max - $n1; $blank > 0; $blank--) { 
+    //         echo " ";
+    //     }
+    //     for ($n2 = 0; $n2 <= $n1 - 1; $n2++) {
+    //         echo "*";
+    //     }
+    //     echo "\n";
+    // }
+
+
+
+
+    // 직각 삼각형 뒤집기
+
+    // $max = 10;
+
+    // for ($n1 = 0; $n1 <= $max; $n1++) 
+    // {
+    //     for ($n2 = 0; $n2 <= $max - $n1; $n2++) 
+    //     { 
+    //         echo "*";
+    //     }
+    //     echo "\n";
+    // }
+
+    // $max = 10;
+    
+    // for ($n1 = 0; $n1 <= $max; $n1++) 
+    // {
+    //     for ($n2 = 0; $n2 <= $max - $n1 ; $n2++) 
+    //     { 
+    //         echo "*";
+    //     }
+    //     echo "\n";
+    // }
+
+
+
+    // 피라미드
+
+    // $max = 10;
+
+    // for ($n1 = 0; $n1 <= $max; $n1++)
+    // { 
+    //     for ($blank = $max - $n1; $blank > 0; $blank--)
+    //     { 
+    //         echo " ";
+    //     }
+    //     for ($n2 = 0; $n2 <= $n1; $n2++) 
+    //     { 
+    //         echo "*";
+    //     }
+    //     for ($n3 = 1; $n3 <= $n1; $n3++) 
+    //     { 
+    //         echo "*";
+    //     }
+    //     echo "\n";
+    // }
+
+
+    // $max = 10;
+    // for ($n1 = 0; $n1 <= $max; $n1++) 
+    // { 
+    //     for ($blank = $max - $n1; $blank > 0; $blank--) 
+    //     { 
+    //         echo " ";
+    //     }
+    //     for ($n2 = 0; $n2 <= $n1; $n2++) 
+    //     { 
+    //         echo "*";
+    //     }
+    //     for ($n3 = 1; $n3 <= $n1; $n3++) 
+    //     { 
+    //         echo "*";
+    //     }
+    //     echo "\n";
+    // }
+
+
+
+    // 마름모
+    // $max = 10;
+    // for ($n1 = 0; $n1 <= $max; $n1++) 
+    // { 
+    //     for ($blank = $max - $n1; $blank > 0; $blank--) 
+    //     { 
+    //         echo " ";
+    //     }
+    //     for ($n2 = 0; $n2 <= $n1; $n2++) 
+    //     { 
+    //         echo "*";
+    //     }
+    //     for ($n3 = 1; $n3 <= $n1; $n3++) 
+    //     { 
+    //         echo "*";
+    //     }
+    //     echo "\n";
+    // }
+    // for ($n1 = 1; $n1 <= $max; $n1++) 
+    // { 
+    //     for ($blank = 0; $blank < $n1; $blank++) 
+    //     { 
+    //         echo " ";
+    //     }
+    //     for ($n2 = 0; $n2 <= $max - $n1; $n2++) 
+    //     { 
+    //         echo "*";
+    //     }
+    //     for ($n3 = 1; $n3 <= $max - $n1; $n3++) 
+    //     { 
+    //         echo "*";
+    //     }
+    //     echo "\n";
+    // }
+
+
+    $max = 10;
+    for ($n1 = 0; $n1 <= $max; $n1++) 
+    { 
+        for ($blank = $max - $n1; $blank > 0; $blank--) 
+        { 
+            echo " ";
+        }
+        for ($n2 = 0; $n2 <= $n1; $n2++) 
+        { 
+            echo "*";
+        }
+        for ($n3 = 1; $n3 <= $n1; $n3++) 
+        { 
+            echo "*";
+        }
+        echo "\n";
+    }
+    for ($n1 = 1; $n1 <= $max; $n1++) 
+    { 
+        for ($blank = 0; $blank < $n1; $blank++) 
+        { 
+            echo " ";
+        }
+        for ($n2 = 0; $n2 <= $max - $n1; $n2++) 
+        { 
+            echo "*";
+        }
+        for ($n3 = 1; $n3 <= $max - $n1; $n3++) 
+        { 
+            echo "*";
+        }
+        echo "\n";
+    }
+?>
