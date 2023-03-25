82a5e4f39c4aa0a5d2e32623f25f64ea03e29c49
LDH1103 <ldh517525@gmail.com>
2023-03-25 14:16:00 +0900

    23-03-25

--- PHP/Training/04_2_tng_loopself2.php
+++ PHP/Training/04_2_tng_loopself2.php
@@ -333,4 +333,42 @@
     //         }
     //     echo("\n");
     // }
+
+
+    // 나무
+    
+    // for ($n1 = 1; $n1 <= $max; $n1++) { 
+    //     for ($blank = $max - $n1; $blank > 0; $blank--) { 
+    //         echo " ";
+    //     }
+    //     for ($n2 = 0; $n2 < $n1; $n2++) 
+    //     { 
+    //         echo "*";
+    //     }
+    //     for ($n3 = 1; $n3 < $n1; $n3++) { 
+    //         echo "*";
+    //     }
+    //     echo "\n";
+    // }   
+    // for ($n1 = 1; $n1 <= $max; $n1++) { 
+    //     for ($blank = $max - $n1; $blank > 0; $blank--) { 
+    //         echo " ";
+    //     }
+    //     for ($n2 = 0; $n2 < $n1; $n2++) 
+    //     { 
+    //         echo "*";
+    //     }
+    //     for ($n3 = 1; $n3 < $n1; $n3++) { 
+    //         echo "*";
+    //     }
+    //     echo "\n";
+    // }
+    // for ($n1 = 0; $n1 < $max - 2; $n1++)
+    // {
+    //     for ($blank = 0; $blank < $max - 3; $blank++)
+    //         echo " ";
+    //     for ($n2 = 0; $n2 < $max - 1; $n2++)
+    //         echo "*";
+    //         echo "\n";
+    // }
 ?>
