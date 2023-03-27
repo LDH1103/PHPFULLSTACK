7ed8f6afe575be81a08d669ebb5f4cab05e31510
ldh1103 <ldh517525@gmail.com>
2023-03-27 09:27:34 +0900

    23-03-27

--- PHP/Training/04_2_tng_loopself3.php
+++ PHP/Training/04_2_tng_loopself3.php
@@ -1,5 +1,34 @@
 <?php
 
+    // $max = 5;
+
+
+    // for ($n1 = 0; $n1 <= $max; $n1++) 
+    // { 
+    //     for ($blank = $max - $n1; $blank > 0; $blank--) 
+    //     { 
+    //         echo " ";
+    //     }
+    //     for ($n2 = 0; $n2 < $n1; $n2++) 
+    //     { 
+    //         echo "★";
+    //     }
+    //     echo "\n";
+    // }
+
+    // for ($n1 = 0; $n1 <= $max; $n1++) 
+    // { 
+    //     for ($blank = $max - $n1; $blank > 0; $blank--) 
+    //     { 
+    //         echo " ";
+    //     }
+    //     for ($n2 = 0; $n2 < $n1; $n2++) 
+    //     { 
+    //         echo "*";
+    //     }
+    //     echo "\n";
+    // }
+
     // $n = 6;
 
     // for ($i = 0; $i < $n; $i += 2) {
