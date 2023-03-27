9de3bb53239946adaebf8f0017104271414d6fa1
ldh1103 <ldh517525@gmail.com>
2023-03-27 13:17:20 +0900

    23-03-27

---
+++ PHP/Example/05_1_ex1_array.php
@@ -0,0 +1,78 @@
+<?php
+
+    $num = 10;
+    $num = 11; // 기존 num의값인 10은 날아가고 11만 저장됨
+
+    // 배열
+
+    $week = array("Sun", "Mon", "Tue", "Wed"); // $week 한 변수에 문자열 4개를 넣음, 순서대로 0번방, 1번방, ...
+    // echo $week[0]; // 0번방에 있는 Sun을 출력
+    // echo "\n";
+    // echo $week[0]." ".$week[3];
+
+    // $mon = "Mon";
+    // $sun = "Sun";
+    // $tue = "Tue";
+    // $wed = "Wed";
+
+    // $week2 = array($sun, $mon, $tue, $wed); // 미리 선언되어있는 변수를 넣을수도 있음
+    // $week3 = array($week[0], $week[1], $week[2], $week[3]); // 선언되어있는 array에서도 가져올수 있음
+
+    // echo $week2[0];
+
+
+    // 멀티타입 배열
+    // 다양한 데이터형식값이 들어갈수있음
+
+    // $arr_mult_type = array("aaa", 1, 3.14, 'a');
+    // var_dump ($arr_mult_type);
+
+
+    // 연상 배열
+
+    // $arr_ass = array("key1" => "val1"
+    //                     , "key2" => "val2"
+    //                     , 3 => "val3" // 키값은 문자열이 아니어도 됨, 보통은 문자열, 보통 데이터형식은 통일
+    //                     , "key4" => "val4");
+    // echo $arr_ass[3]; // key1에 해당하는 값을 출력
+
+
+    // 다차원 배열
+
+    // $arr_temp = array(
+    //                 array(1, 2, 3, 4)
+    //                 , array(5, 6, 7, 8)
+    //                 , array(
+    //                     array(9, 10, 11)
+    //                     // , array(12, 13, 14)
+    //                 )
+    //             );
+    
+    // echo $arr_temp[0][1]; // 2를출력
+    // echo $arr_temp[1][3]; // 8을 출력
+
+    // echo $arr_temp[2][0][1]; // 10을 출력
+    // echo $arr_temp[2][0][0]; // 9를 출력
+    // echo $arr_temp[2][1][2]; // 14를 출력
+
+    // $arr_temp_3 = $arr_temp[2][0]; // 현업 방식 : 미리 필요한 배열을 변수에 담고, 그 변수를 이용해 데이터를 뽑아옴
+    // var_dump($arr_temp_3);
+    // echo $arr_temp_3[2];
+
+    // $arr_temp_3 = $arr_temp[2];
+    // // var_dump($arr_temp_3);
+    // print_r($arr_temp_3);
+    // $arr_temp_3_c = array(
+    //                     array(9, 10, 11)
+    //                     // , array(12, 13, 14);
+    //                 );
+
+
+    // 배열의 원소 삭제 : unset()
+    // 키값자체가 삭제됨(2번방을 삭제한다고 해도 3번방이 2번방으로 땡겨지는게 아님, 2번방은 빈자리로 둠)
+    $arr_week = array("Sun", "Mon", "delete", "Tue", "Wed");
+    unset($arr_week[2]);
+    print_r($arr_week);
+
+
+?>
---
+++ PHP/Training/05_1_tng1_array.php
@@ -0,0 +1,47 @@
+<?php
+
+    // 음식 종류 5개 이상을 배열로 만들기
+    $food = array("짜장면", "탕수육", "햄버거", "피자", "치킨");
+
+    // echo $food[4].", ".$food[1];
+
+    // array $food에서 무작위로 값을 출력
+
+    // $rand_num = rand(0, 4);
+    // echo $food[$rand_num];
+
+    // $random = array_rand($food);
+    // echo $food[$random];
+    
+
+    // 배열 순서를 Mon, Wed, Sun, Tue가 되도록 week2를 변경
+
+    // $week = array("Sun", "Mon", "Tue", "Wed");
+    // $week2 = array($week[1], $week[3], $week[0], $week[2]);
+
+    // echo $week2[0]." ".$week2[1]." ".$week2[2]." ".$week2[3];
+    // print_r($week2);
+
+
+    // 연상 배열
+    // 키는 요리명, 값은 주재료 하나로 이루어진 배열을 만들기 (배열 길이는 4)
+
+    // $arr_food = array("탕수육" => "돼지고기"
+    //                     , "햄버거" => "패티"
+    //                     , "피자" => "치즈"
+    //                     , "치킨" => "닭고기");
+    // echo $arr_food["피자"];
+
+
+    // 키 : 김치 원소를 삭제
+
+    $arr_food_del = array(
+                        "된장찌개" => "파"
+                        , "볶음밥" => "양파"
+                        , "김치" => "마늘"
+                        , "비빔밥" => "참기름"
+                    );
+    unset($arr_food_del["김치"]);
+    print_r($arr_food_del);
+
+?>
