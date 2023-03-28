60111f999f5e87aa3315a7e0ec15ea7c5ae4b07f
ldh1103 <ldh517525@gmail.com>
2023-03-28 13:14:20 +0900

    23-03-28

--- PHP/Example/06_1_ex1_function.php
+++ PHP/Example/06_1_ex1_function.php
@@ -27,18 +27,31 @@
 
     // 가변 파라미터 함수
 
-    function fnc_args_add() {
-        $args = func_get_args(); // 가변 파라미터 습득
-        $sum = 0; // 더하기 결과 저장 변수
+    // function fnc_args_add() {
+    //     $args = func_get_args(); // 가변 파라미터 습득
+    //     $sum = 0; // 더하기 결과 저장 변수
         
-        // 루프 : 더하기 실행
-        foreach ($args as $val) {
-            $sum += $val;
+    //     // 루프 : 더하기 실행
+    //     foreach ($args as $val) {
+    //         $sum += $val;
+    //     }
+        
+    //     return $sum;
+    // }
+
+    // echo fnc_args_add(1, 2, 3, 4);
+
+
+
+    // 재귀함수
+    
+    function rcc($param_a) {
+        if ($param_a === 1) {
+            return 1;
         }
-        
-        return $sum;
+        return $param_a * rcc($param_a - 1);
     }
 
-    echo fnc_args_add(1, 2, 3, 4);
+    echo rcc(3); // = 3!
 
 ?>
