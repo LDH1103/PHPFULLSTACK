2e18f2479580dfeea82a0b5928c644405184edd4
ldh1103 <ldh517525@gmail.com>
2023-03-29 10:02:19 +0900

    23-03-29

---
+++ PHP/Example/fn.php
@@ -0,0 +1,22 @@
+<?php
+
+    // void 함수 (리턴값이 없는)
+
+    function sum($n1, $n2) {
+        echo $n1 + $n2."\n";
+        // return; return을 생략해도 자동으로 들어가있지만, return값은 없음
+    }
+
+    // sum(1, 2);
+    // sum(2, 4);
+
+    // return 함수 (리턴값이 있는)
+
+    function sum2($n1, $n2) {
+        return $n1 + $n2;
+    }
+
+    $result = sum2(3, 6); // 3+6인 9가 sum2의 값으로 바뀜, echo sum2(3, 6);을 해보면 9가나옴
+    echo $result."\n"; // 추가 응용 가능
+
+?>
--- PHP/Training/06_99_total.php
+++ PHP/Training/06_99_total.php
@@ -4,14 +4,41 @@
 
     // 별 출력 함수
 
+    // function print_star($a) {
+    //     for ($n1 = 0; $n1 < $a; $n1++) { 
+    //         echo "*";
+    //     }
+    //     echo "\n";
+    // }
+
     function print_star($a) {
-        for ($n1 = 0; $n1 < $a; $n1++) { 
+        for ($n1 = 0; $n1 < $a; $n1++) {
             echo "*";
         }
         echo "\n";
     }
 
+    // print_star(5);
+
     // 직각삼각형
+
+    // function star_no($a) {
+    //     $no = 1;
+    //     for ($n1 = 0; $n1 < $a; $n1++) {
+    //         print_star($no);
+    //         $no++;
+    //     }
+    // }
+    // star_no(5);
+
+    // function star_no($a) {
+    //     $no = 1;
+    //     for ($n1 = 0; $n1 < $a; $n1++) {
+    //         print_star($no);
+    //         $no++;
+    //     }
+    // }
+    //     star_no(5);
 
     function star_no($a) {
         $no = 1;
@@ -19,10 +46,11 @@
             print_star($no);
             $no++;
         }
+        echo "\n";
     }
     star_no(5);
 
-    echo "\n";
+
 
     // ㅁ 모양
 
@@ -30,12 +58,26 @@
         for ($n1 = 0; $n1 < $a; $n1++) {
             print_star($a);
         }
+        echo "\n";
     }
     star_no2(5);
 
-    echo "\n";
 
     // >모양
+
+    // function star_no3($a) {
+    //     $no = 0;
+    //     for ($n1 = 0; $n1 < $a; $n1++) {
+    //         print_star($no);
+    //         $no++;
+    //     }
+    //     for ($n2 = 0; $n2 < $a; $n2++) {
+    //         print_star($no);
+    //         $no--;
+    //     }
+    //     echo "\n";
+    // }
+    // star_no3(5);
 
     function star_no3($a) {
         $no = 0;
