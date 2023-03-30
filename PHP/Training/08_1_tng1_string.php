08774314bba34af00ed3b5da8eabe85b75dd3e59
ldh1103 <ldh517525@gmail.com>
2023-03-30 12:24:16 +0900

    23-03-30

--- PHP/Training/08_1_tng1_string.php
+++ PHP/Training/08_1_tng1_string.php
@@ -29,34 +29,67 @@
 
     // I am always Hello. 에서 "Hello"를 "Happy"로 바꾸어 출력
 
-    define("STR", "I am always %s.");
-    printf (STR, "Hello");
-    echo "\n";
-    printf (STR, "Happy");
-    echo "\n";
+    // define("STR", "I am always %s.");
+    // printf (STR, "Hello");
+    // echo "\n";
+    // printf (STR, "Happy");
+    // echo "\n";
     
-    $str_1 = "I am always Hello.";
-    echo mb_substr($str_1, 0, 12)."Happy."."\n"; 
 
-    $str_expl = "I am always Hello.";
-    $arr_expl = explode(" ", $str_expl);
-    $arr_expl[3] = "Happy.";
-
-    $str_impl = implode(" ", $arr_expl);
-    echo $str_impl."\n";
+    // $str_1 = "I am always Hello.";
+    // echo mb_substr($str_1, 0, 12)."Happy."."\n"; 
+    // echo mb_substr($str_1, -18, -6)."Happy."."\n"; 
 
 
+    // $str_expl = "I am always Hello.";
+    // $arr_expl = explode(" ", $str_expl);
+    // $arr_expl[3] = "Happy.";
 
-    // 문자열로 된 숫자 2개를 정수로 변환하여 더한 후 다시 문자열로 변환하여 출력
+    // $str_impl = implode(" ", $arr_expl);
+    // echo $str_impl."\n";
 
-    $num1 = "314";
-    $num2 = "615";
-    $num3 = (int)$num1 + (int)$num2;
-    $num3 = (string)$num3;
 
-    $num4 = $num1 + $num2;
+    // $str_all = "I am always Hello.";
+    // $str_expl = explode("Hello", $str_all);
+    // $str_impl = implode("Happy", $str_expl);
+    // echo $str_impl;
 
-    var_dump($num3);
+    // 함수명 : my_str_replace
+    // 리턴값 : String $result_str
 
+
+
+    function my_str_replace($a) {
+        $str_all = $a;
+        $str_expl = explode("Hello", $str_all);
+        $result_str = implode("Happy", $str_expl);
+        echo $result_str;
+    }
+
+    my_str_replace("I am always Hello.");
+
+
+    // // 문자열로 된 숫자 2개를 정수로 변환하여 더한 후 다시 문자열로 변환하여 출력
+
+    // $num1 = "314";
+    // $num2 = "615";
+    // $num3 = (int)$num1 + (int)$num2;
+    // $num3 = (string)$num3;
+
+    // $num4 = $num1 + $num2;
+
+    // var_dump($num3);
+    // echo "\n";
+
+
+    // 반복문을 이용하며 뒤에서부터 한글자씩 출력
+    // "Hello PHP !!"
+
+    // $arr_expl = explode("/", "H/e/l/l/o/ /P/H/P/ /!/!");
+
+    // $n1 = count($arr_expl);
+    // foreach ($arr_expl as $val) {
+    //     echo $arr_expl[count($arr_expl)];
+    // }
 
 ?>
