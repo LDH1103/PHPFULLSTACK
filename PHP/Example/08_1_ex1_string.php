3537fe0f4700d1e07ad2cfc0baef4e4e65c41cee
ldh1103 <ldh517525@gmail.com>
2023-03-30 11:38:06 +0900

    23-03-30

--- PHP/Example/08_1_ex1_string.php
+++ PHP/Example/08_1_ex1_string.php
@@ -18,12 +18,75 @@
 
 
     // URL 관련 함수
-    $url = "https://www.google.com/search?q=PHP+rand&source=hp&ei=m_4jZMa4FKvg2roPvfGWwA4&iflsig=AOEireoAAAAAZCQMq-kkf6r2eXgW1x60ixcQJDsuc-mw&oq=&gs_lcp=Cgdnd3Mtd2l6EAEYADIXCAAQigUQ6gIQtAIQigMQtwMQ1AMQ5QIyFwgAEIoFEOoCELQCEIoDELcDENQDEOUCMhcIABCKBRDqAhC0AhCKAxC3AxDUAxDlAjIXCAAQigUQ6gIQtAIQigMQtwMQ1AMQ5QIyFAgAEIoFEOoCELQCEIoDELcDEOUCMhcIABCKBRDqAhC0AhCKAxC3AxDUAxDlAjIXCAAQigUQ6gIQtAIQigMQtwMQ1AMQ5QIyFwgAEIoFEOoCELQCEIoDELcDENQDEOUCMhcILhCKBRDqAhC0AhCKAxC3AxDUAxDlAjIUCAAQigUQ6gIQtAIQigMQtwMQ5QJQAFgAYJwLaAFwAHgAgAEAiAEAkgEAmAEAsAEK&sclient=gws-wiz";
 
-    $arr_url = parse_url($url); // 긴 URL을 분할해줌
-    // var_dump($arr_url);
+    // $url = "https://www.google.com/search?q=PHP+rand&source=hp&ei=m_4jZMa4FKvg2roPvfGWwA4&iflsig=AOEireoAAAAAZCQMq-kkf6r2eXgW1x60ixcQJDsuc-mw&oq=&gs_lcp=Cgdnd3Mtd2l6EAEYADIXCAAQigUQ6gIQtAIQigMQtwMQ1AMQ5QIyFwgAEIoFEOoCELQCEIoDELcDENQDEOUCMhcIABCKBRDqAhC0AhCKAxC3AxDUAxDlAjIXCAAQigUQ6gIQtAIQigMQtwMQ1AMQ5QIyFAgAEIoFEOoCELQCEIoDELcDEOUCMhcIABCKBRDqAhC0AhCKAxC3AxDUAxDlAjIXCAAQigUQ6gIQtAIQigMQtwMQ1AMQ5QIyFwgAEIoFEOoCELQCEIoDELcDENQDEOUCMhcILhCKBRDqAhC0AhCKAxC3AxDUAxDlAjIUCAAQigUQ6gIQtAIQigMQtwMQ5QJQAFgAYJwLaAFwAHgAgAEAiAEAkgEAmAEAsAEK&sclient=gws-wiz";
 
-    parse_str($arr_url["query"], $arr_parse);
-    var_dump($arr_parse);
+    // $arr_url = parse_url($url); // 긴 URL을 분할해줌
+    // // var_dump($arr_url);
+
+    // parse_str($arr_url["query"], $arr_parse);
+    // var_dump($arr_parse);
+
+
+    // 역순의 문자열
+
+    // $str_abcd = "abcd";
+    // echo strrev($str_abcd); // 문자열 뒤집기, 한글은 안됨
+
+
+    // 문자열 자르기
+    
+    // $str_1 = "가나다라마";
+    // echo substr($str_1, 3); // 한글을 자르려면 한글자당 3byte
+    // echo mb_substr($str_1, 3); // 멀티 바이트(알파벳이 아닌 글자)를 글자수대로 자를려면 mb_substr, 알파벳도 가능
+    // echo mb_substr($str_1, 2, 1); // 3번째 글자부터 1개만 남겨두기
+    // echo mb_substr($str_1, -1); // 음수도 가능 (뒤에서부터 자르기)
+
+
+    // 문자열 앞뒤 빈공간 지우기
+
+    // $str_trim = "          abcd         \n";
+    // echo trim($str_trim); // rtrim, ltrim
+    // echo "aaa"; // 계행 체크용
+
+
+    // 문자열의 길이를 구하는 함수
+
+    // $str_len = "가나다라";
+    // echo strlen($str_len); // 한글은 3byte기 떄문에 12가 출력
+    // echo mb_strlen($str_len);
+
+
+    // 문자열 포맷에 따라 출력하는 함수
+
+    // printf("안녕하세요. %s입니다. %d", "PHP", 1234); // %s : 문자열, %d : 정수
+    
+    // define("WELCOME", "안녕하세요. %s님.");
+    // printf(WELCOME, "홍길동");
+    
+
+    // 문자열을 분리하는 함수
+
+    // $str_sscanf = "가나다라 50 abcd";
+    // sscanf($str_sscanf, "%s %d %s", $str_ko, $int_d, $str_en);
+    // echo $str_ko, "\n", $int_d, "\n", $str_en, "\n";
+    
+
+    // 문자열을 반복해서 찍어주는 함수
+
+    // echo str_repeat("가나", 3);
+
+
+    // 문자열을 특정문자열로 분리하는 함수 : explode()
+
+    $str_expl = "홍길동,27세,남자,의적,조선";
+    $arr_expl = explode(",", $str_expl); // explode(구분할 문자열, 문자열이 담긴 변수)
+    // print_r($arr_expl);
+
+
+    // 배열을 특정 문자열로 합치는 함수 : implode()
+
+    $str_impl = implode(" / ", $arr_expl);
+    echo $str_impl;
 
 ?>
---
+++ PHP/Training/08_1_tng1_string.php
@@ -0,0 +1,60 @@
+<?php
+
+    // 문자열 자르기로 "PHP입니다."만 출력
+
+    // $str_tng = "안녕하세요. PHP입니다.";
+    // echo mb_substr($str_tng, 7)."\n";
+    // echo mb_substr($str_tng, -7)."\n";
+
+    // // "세요. P"만 출력
+
+    // echo mb_substr($str_tng, 3, 5)."\n";
+    // echo mb_substr($str_tng, 3, -6)."\n";
+    // echo mb_substr($str_tng, -11, 5)."\n";
+
+
+    // 기본 포맷 : ERROR(숫자) : XXX ERROR가 발생했습니다.
+    // 에러 번호 : 에러 종류
+    // 1 : 시스템
+    // 2 : 로그인
+    // 3: 접속 
+
+    // define("ERROR_MSG", "ERROR(%d) : %s ERROR가 발생했습니다.");
+    // printf(ERROR_MSG, 1, "시스템");
+    // echo "\n";
+    // printf(ERROR_MSG, 2, "로그인");
+    // echo "\n";
+    // printf(ERROR_MSG, 3, "접속");
+
+
+    // I am always Hello. 에서 "Hello"를 "Happy"로 바꾸어 출력
+
+    define("STR", "I am always %s.");
+    printf (STR, "Hello");
+    echo "\n";
+    printf (STR, "Happy");
+    echo "\n";
+    
+    // $str_1 = "I am always Hello.";
+    // echo mb_substr($str_1, 0, 12)."Happy."."\n"; 
+
+    $str_expl = "I am always Hello.";
+    $arr_expl = explode(" ", $str_expl);
+    $arr_expl[3] = "Happy.";
+
+    $str_impl = implode(" ", $arr_expl);
+    echo $str_impl."\n";
+
+
+
+    // 문자열로 된 숫자 2개를 정수로 변환하여 더한 후 다시 문자열로 변환하여 출력
+
+    $num1 = "314";
+    $num2 = "615";
+    $num3 = (int)$num1 + (int)$num2;
+    $num3 = (string)$num3;
+
+    var_dump($num3);
+
+
+?>
