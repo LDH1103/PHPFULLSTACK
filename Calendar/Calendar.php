c242373535e0a41c4925d5d110628524a0d2e09b
ldh1103 <ldh517525@gmail.com>
2023-03-31 18:07:22 +0900

    23-03-31

--- Calendar/Calendar.php
+++ Calendar/Calendar.php
@@ -1,78 +1,83 @@
 <?php 
-	// GET으로 넘겨 받은 year값이 있다면 넘겨 받은걸 year변수에 적용하고 없다면 현재 년도
-	$year = isset($_GET['year']) ? $_GET['year'] : date('Y');
-	// GET으로 넘겨 받은 month값이 있다면 넘겨 받은걸 month변수에 적용하고 없다면 현재 월
-	$month = isset($_GET['month']) ? $_GET['month'] : date('m');
+$date = empty($_GET['date']) ? date('Y-m-d') : $_GET['date'];
+$firstDate = date('Y-m-01',strtotime($date));
+$firstDateWcode = date('w',strtotime($firstDate));
+$lastDateDay = date('t', strtotime($date));
+$lastDateWcode = date('w',strtotime( date('Y-m-'.$lastDateDay,strtotime($date)) ));
 
-	$date = "$year-$month-01"; // 현재 날짜
-	$time = strtotime($date); // 현재 날짜의 타임스탬프
-	$start_week = date('w', $time); // 1. 시작 요일
-	$total_day = date('t', $time); // 2. 현재 달의 총 날짜
-	$total_week = ceil(($total_day + $start_week) / 7);  // 3. 현재 달의 총 주차
-?>
+$prevDate = date('Y-m-d',strtotime("-1 month",strtotime($firstDate)));
+$nextDate = date('Y-m-d',strtotime("+1 month",strtotime($firstDate)));
+$prevLastDateDay = date('t', strtotime($prevDate));
 
-<!DOCTYPE html>
-<html lang="ko">
-<head>
-	<meta charset="UTF-8">
-	<title>calendar</title>
-	<style type="text/css">
-		table {
-			border-spacing: 0;
+$week = array();
+$day = 1;
+while(1){
+	$subWeek = array();
+	for($i=0;$i<7;$i++){
+		if( $day > $lastDateDay){ 
+
+			// 마지막 날의 날짜코드가 6이 아니라면, 다음달을 미리 넣어준다.
+			if( $lastDateWcode != 6){
+				for($si=0;$si < 6-$lastDateWcode; $si++){
+					$thisDate = date('Y-m-'.($si+1),strtotime("+1 month",strtotime($firstDate)));
+					$subWeek[] = array('type'=>'next', 'day'=>$si+1,'date'=>$thisDate,'wcode'=>date('w',strtotime($thisDate)));		
+				}				
+			}
+			break; 
 		}
-		table td {
-            padding : 10px;
-			text-align: center;
+		if( $day == 1 && $firstDateWcode != 0){
+			for($si=($prevLastDateDay-$firstDateWcode);$si < $prevLastDateDay; $si++){
+				$thisDate = date('Y-m-'.($si+1),strtotime("-1 month",strtotime($firstDate)));
+				$subWeek[] = array('type'=>'prev', 'day'=>$si+1,'date'=>$thisDate,'wcode'=>date('w',strtotime($thisDate)));
+			}
+			$i += $firstDateWcode;
 		}
-	</style>
-</head>
+
+		$thisDate = date('Y-m-'.$day,strtotime($firstDate));
+		$subWeek[] = array('type'=>'now', 'day'=>$day,'date'=>$thisDate,'wcode'=>date('w',strtotime($thisDate)));		
+		$day++;
+	}
+	$week[] = $subWeek;
+	if( $day > $lastDateDay){ break; }
+}
+
+echo '
 <body>
-	<?php echo "$year 년 $month 월" ?>
-	<!-- 현재가 1월이라 이전 달이 작년 12월인경우 -->
-	<?php if ($month == 1): ?>
-		<!-- 작년 12월 -->
-		<a href="/?year=<?php echo $year-1 ?>&month=12">이전 달</a>
-	<?php else: ?>
-		<!-- 이번 년 이전 월 -->
-		<a href="/?year=<?php echo $year ?>&month=<?php echo $month-1 ?>">이전 달</a>
-	<?php endif ?>
+	<div class="action">
+		<a href="?date='.$prevDate.'">이전달</a>
+		<strong>'.$date.'</strong>
+		<a href="?date='.$nextDate.'">다음달</a>
+	</div>
+	<table>
+		<tr>
+			<th>일</th>
+			<th>월</th>
+			<th>화</th>
+			<th>수</th>
+			<th>목</th>
+			<th>금</th>
+			<th>토</th>
+		</tr>
+';
 
-	<!-- 현재가 12월이라 다음 달이 내년 1월인경우 -->
-	<?php if ($month == 12): ?>
-		<!-- 내년 1월 -->
-		<a href="/?year=<?php echo $year+1 ?>&month=1">다음 달</a>
-	<?php else: ?>
-		<!-- 이번 년 다음 월 -->
-		<a href="/?year=<?php echo $year ?>&month=<?php echo $month+1 ?>">다음 달</a>
-	<?php endif ?>
-
-
-	<table border="1">
-		<tr> 
-			<th>일</th> 
-			<th>월</th> 
-			<th>화</th> 
-			<th>수</th> 
-			<th>목</th> 
-			<th>금</th> 
-			<th>토</th> 
-		</tr> 
-
-		<!-- 총 주차를 반복합니다. -->
-		<?php for ($n = 1, $i = 0; $i < $total_week; $i++): ?> 
-			<tr> 
-				<!-- 1일부터 7일 (한 주) -->
-				<?php for ($k = 0; $k < 7; $k++): ?> 
-					<td> 
-						<!-- 시작 요일부터 마지막 날짜까지만 날짜를 보여주도록 -->
-						<?php if ( ($n > 1 || $k >= $start_week) && ($total_day >= $n) ): ?>
-							<!-- 현재 날짜를 보여주고 1씩 더해줌 -->
-							<?php echo $n++ ?>
-						<?php endif ?>
-					</td> 
-				<?php endfor; ?> 
-			</tr> 
-		<?php endfor; ?> 
-	</table>
-</body>
-</html>
+foreach($week as $k=>$v){
+	echo '<tr>';
+	foreach($v as $sk=>$sv){
+		echo '<td class="wcode_'.$sv['wcode'].''.($sv['type'] != 'now' ? ' prevnext':'').'" title="'.$sv['date'].'">'.$sv['day'].'</td>';
+	}
+	echo '</tr>';
+}
+echo '</body>';
+?>
+<style>
+	body{  width:450px; }
+	.action{ text-align:center; margin:10px 0; }
+	.action strong{ font-size:45px; }
+	.action a{ text-decoration: none; color:#333; }
+	h1{ text-align:center; }
+	table{ width:100%;}
+	table th,td{ width: 14%; text-align:center; }
+	table th.wcode_0,table td.wcode_0{ color:red; }
+	table th.wcode_6,table td.wcode_6{ color:blue; }
+	table td.prevnext{ color:#eee; }
+</style>
