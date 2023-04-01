9971109e365d00249ba3957bdf9b3f45be519b8a
LDH1103 <ldh517525@gmail.com>
2023-04-01 19:08:07 +0900

    23-04-01

---
+++ PHP/Training/tng_GPT.php
@@ -0,0 +1,56 @@
+<?php
+
+// // "Hello, world!"를 출력하는 PHP 코드를 작성하세요.
+// echo "Hello, world!";
+
+// // 변수 $name에 "Alice"라는 문자열을 할당하고, 이를 출력하는 PHP 코드를 작성하세요.
+// $name = "Alice";
+// echo $name;
+
+// // 변수 $num1과 $num2에 각각 5와 7을 할당하고, 이 두 변수를 더한 결과를 출력하는 PHP 코드를 작성하세요.
+// $num1 = 5;
+// $num2 = 7;
+// echo $num1 + $num2;
+
+// // 배열 $fruits에 "apple", "banana", "orange"를 순서대로 저장하고, 이 배열을 반복문을 사용하여 출력하는 PHP 코드를 작성하세요.
+// $fruits = array("apple", "banana", "orange");
+// foreach ($fruits as $val) {
+//     echo $val."\n";
+// }
+
+// // 조건문을 사용하여 변수 $age가 18 이상이면 "성인입니다"를, 18 미만이면 "미성년자입니다"를 출력하는 PHP 코드를 작성하세요.
+// $age = 18;
+// if ($age < 18) {
+//     echo "미성년자 입니다";
+// } else {
+//     echo "성인 입니다";
+// }
+
+
+
+// 주어진 문자열에서 모든 단어의 첫 글자를 대문자로 변경하는 PHP 함수를 작성하세요. (예: "hello world" -> "Hello World")
+// $str = "hello world";
+// echo ucfirst($str);
+
+// 주어진 정수 배열에서 가장 큰 값과 가장 작은 값을 출력하는 PHP 코드를 작성하세요.
+// $num = array(1, 2, 3, 4, 10, 5, 0);
+// echo max($num)."\n";
+// echo min($num);
+
+// 주어진 연관 배열에서 "name"이라는 키의 값을 "John"에서 "Alice"로 변경하는 PHP 코드를 작성하세요.
+// $info = array(
+//     "name" => "John",
+//     "age" => 30,
+//     "email" => "john@example.com"
+// );
+
+// foreach ($info as $key => $val) {
+//     if ($key === "name") {
+//         $info["name"] = "Alice";
+//     }
+// }
+// var_dump($info);
+
+
+
+?>
