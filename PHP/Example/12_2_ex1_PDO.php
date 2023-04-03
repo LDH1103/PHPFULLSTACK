99183841c7758afc4cbcdfc7fc433be4b3fae672
ldh1103 <ldh517525@gmail.com>
2023-04-03 16:02:38 +0900

    23-04-03

--- PHP/Example/12_2_ex1_PDO.php
+++ PHP/Example/12_2_ex1_PDO.php
@@ -18,38 +18,43 @@
     // PDO Class로 DB 연동
     $obj_conn = new PDO( $db_dns, $db_user, $db_password, $db_option );
 
+
+
+    // SELECT 예제
+
     // 사번 10001, 100002의 현재 연봉과 사번, 생일을 가져오는 쿼리를 작성
-    $sql = 
-        " SELECT "
-        ."      emp.emp_no "
-        ."      ,sal.salary "
-        ."      ,emp.birth_date "
-        ." FROM " 
-        ."      employees emp "
-        ."          INNER JOIN salaries sal "
-        ."          ON emp.emp_no = sal.emp_no "
-        ." WHERE " 
-        ."      ( "
-        ."          emp.emp_no = :emp_no1 " // 컬럼명과 이름을 맞춰주기 위해 :emp_no
-        ."          OR emp.emp_no = :emp_no2 "
-        ."      ) "
-        ."      AND sal.to_date = DATE(99990101) ";
+    // $sql = 
+    //     " SELECT "
+    //     ."      emp.emp_no "
+    //     ."      ,sal.salary "
+    //     ."      ,emp.birth_date "
+    //     ." FROM " 
+    //     ."      employees emp "
+    //     ."          INNER JOIN salaries sal "
+    //     ."          ON emp.emp_no = sal.emp_no "
+    //     ." WHERE " 
+    //     ."      ( "
+    //     ."          emp.emp_no = :emp_no1 " // 컬럼명과 이름을 맞춰주기 위해 :emp_no
+    //     ."          OR emp.emp_no = :emp_no2 "
+    //     ."      ) "
+    //     ."      AND sal.to_date = DATE(99990101) ";
 
-    $arr_prepare = 
-        array(
-            ":emp_no1" => 10001
-            , ":emp_no2" => 10002
-        );
+    // $arr_prepare = 
+    //     array(
+    //         ":emp_no1" => 10001
+    //         , ":emp_no2" => 10002
+    //     );
 
-    $stmt = $obj_conn->prepare( $sql ); // Prepare Statement를 생성
-    $stmt->execute( $arr_prepare ); // 쿼리 실행
-    $result = $stmt->fetchAll(); // 쿼리 결과를 fetch
-    // var_dump( $result );
+    // $stmt = $obj_conn->prepare( $sql ); // Prepare Statement를 생성
+    // $stmt->execute( $arr_prepare ); // 쿼리 실행
+    // $result = $stmt->fetchAll(); // 쿼리 결과를 fetch
+    // // var_dump( $result );
 
-    foreach ($result as $val) {
-        echo $val["emp_no"]." : $".$val["salary"].", ".$val["birth_date"]."\n";
-    }
+    // foreach( $result as $val ) {
+    //     echo $val["emp_no"]." : $".$val["salary"].", ".$val["birth_date"]."\n";
+    // }
 
+    // $stmt->fetchAll()의 결과
     // $result =
     //     array (
     //         array (
@@ -64,6 +69,33 @@
     //         )
     //     );
 
+
+
+    // INSERT 예제
+
+    $sql = 
+        " INSERT INTO departments( "
+        ."  dept_no "
+        ."  ,dept_name "
+        ." ) "
+        ." VALUES( "
+        ."  :dept_no "
+        ."  ,:dept_name "
+        ." ) ";
+
+    $arr_prepare =
+        array (
+            ":dept_no" => "d011"
+            ,":dept_name" => "PHP풀스택"
+        );
+    
+    $stmt = $obj_conn->prepare( $sql );
+    $result = $stmt->execute( $arr_prepare );
+    $obj_conn->commit();
+    var_dump( $result );
+
     $obj_conn = null; // DB연동 끊기
+
+    // FLUSH PRIVILEGES; 하이디sql 새로고침
 
 ?>
