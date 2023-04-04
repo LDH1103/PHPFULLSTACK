c284c6afde7c0cdbe66490c5b372aa038c84874c
ldh1103 <ldh517525@gmail.com>
2023-04-04 12:32:29 +0900

    23-04-04

--- PHP/Example/404_1_ex1_try_catch.php
+++ PHP/Example/404_1_ex1_try_catch.php
@@ -34,6 +34,33 @@
 
 
     //
+    // try {
+    //     $obj_conn = null;
+    //     my_db_conn( $obj_conn );
+    //     $sql = " SELECT * FROM employees WHERE emp_no = 1000000 ";
+    //     $stmt = $obj_conn->query( $sql );
+    //     $result = $stmt->fetchAll();
+
+    //     // 로그인 했을때 해당 유저가 없을경우, 페이탈 에러가 아니기 때문에 catch문에 들어가지 않음, 강제로 에러 일으키기
+    //     if( count( $result ) === 0 ) {
+    //         // throw Exception : 에러를 강제로 일으키는 구문
+    //         throw new Exception( "쿼리 결과 0건" );
+    //     }
+
+    //     var_dump( $result );
+    //     echo "Try\n";
+    // } catch( Exception $e ) {
+    //     echo "----에러 발생----\n";
+    //     echo $e->getMessage();
+    //     echo "\n----에러 발생----\n";
+    // } finally {
+    //     echo "Finally\n";
+    //     $obj_conn = null;
+    // }
+    // echo "종료";
+
+
+    // 데이터 0건일경우 "데이터 0건", 다른 에러일경우 에러메세지 출력
     try {
         $obj_conn = null;
         my_db_conn( $obj_conn );
@@ -41,28 +68,24 @@
         $stmt = $obj_conn->query( $sql );
         $result = $stmt->fetchAll();
 
-        // 로그인 했을때 해당 유저가 없을경우, 페이탈 에러가 아니기 때문에 catch문에 들어가지 않음, 강제로 에러 일으키기
         if( count( $result ) === 0 ) {
-            // throw Exception : 에러를 강제로 일으키는 구문
-            throw new Exception( "쿼리 결과 0건" );
+            throw new Exception( "E99" );
         }
 
         var_dump( $result );
         echo "Try\n";
     } catch( Exception $e ) {
-        echo "----에러 발생----\n";
-        echo $e->getMessage();
-        echo "\n----에러 발생----\n";
+        if ( $e->getMessage() === "E99" ) {
+            echo "데이터 0건\n";
+        } else {
+            echo "----에러 발생----\n";
+            echo $e->getMessage();
+            echo "\n----에러 발생----\n";
+        }
     } finally {
         echo "Finally\n";
         $obj_conn = null;
     }
     echo "종료";
-
-
-
-
-
-
 
 ?>
---
+++ PHP/Training/404_1_tng1_try_catch.php
@@ -0,0 +1,104 @@
+<?php
+
+    include_once("../Example/12_2_ex2_fnc_db_conn.php");
+
+    // 아래 쿼리를 이용해서 DB 접속 > data획득 후 출력
+    // try-catch로 에러 처리
+    // 에러가 날 경우의 해당 Exception의 에러 메세지를 출력
+    // 정상 종료일 경우 "정상 종료" 출력
+
+    // $sql1 = " SELECT * FROM department ";
+
+    // try {
+    //     $obj_conn = null;
+    //     my_db_conn( $obj_conn );
+    //     $stmt = $obj_conn->query( $sql1 );
+    //     $result = $stmt->fetchAll();
+    //     var_dump( $result );
+    //     echo "정상 종료\n";
+    // } catch( Exception $e ) {
+    //     echo " ---- 에러 발생 ---- \n";
+    //     echo $e->getMessage();
+    //     echo "\n ---- 에러 발생 ---- \n";
+    // } finally {
+    //     $obj_conn = null;
+    // }
+
+
+
+    // $sql2 = " SELECT * FROM dept_manager ";
+
+    // try {
+    //     $obj_conn = null;
+    //     my_db_conn( $obj_conn );
+    //     $stmt = $obj_conn->query( $sql2 );
+    //     $result = $stmt->fetchAll();
+    //     var_dump( $result );
+    //     echo "정상 종료";
+    // } catch( Exception $e ) {
+    //     echo " ---- 에러 발생 ---- \n";
+    //     echo $e->getMessage();
+    //     echo "\n ---- 에러 발생 ---- \n";
+    // } finally {
+    //     $obj_conn = null;
+    // }
+
+
+
+
+
+    // 한번에 하기
+    $sql1 = " SELECT * FROM department ";
+    $sql2 = " SELECT * FROM dept_manager ";
+
+    try {
+        $obj_conn = null;
+        my_db_conn( $obj_conn );
+
+        $stmt_1 = $obj_conn->query( $sql1 );
+        $result_1 = $stmt_1->fetchAll();
+        var_dump( $result_1 );
+
+        $stmt_2 = $obj_conn->query( $sql2 );
+        $result_2 = $stmt_2->fetchAll();
+        var_dump( $result_2 );
+
+        echo "정상 종료";
+    } catch( Exception $e ) {
+        echo " ---- 에러 발생 ---- \n";
+        echo $e->getMessage();
+        echo "\n ---- 에러 발생 ---- \n";
+    } finally {
+        $obj_conn = null;
+    }
+
+    // try {
+    //     $obj_conn = null;
+    //     my_db_conn( $obj_conn );
+
+    //     $stmt_1 = $obj_conn->query( $sql1 );
+    //     $result_1 = $stmt_1->fetchAll();
+    //     var_dump( $result_1 );
+
+    //     $stmt_2 = $obj_conn->query( $sql2 );
+    //     $result_2 = $stmt_2->fetchAll();
+    //     var_dump( $result_2 );
+
+    // } catch( Exception $e ) {
+    //     echo " ---- 에러 발생 ---- \n";
+    //     echo $e->getMessage();
+    //     echo "\n ---- 에러 발생 ---- \n";
+    // } finally {
+    //     if( empty( $e ) ) {
+    //         echo "정상 종료";
+    //     }
+    //     $obj_conn = null;
+    // }
+
+
+
+
+
+
+
+?>
