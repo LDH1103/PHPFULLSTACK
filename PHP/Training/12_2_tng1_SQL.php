024bb8d0383a7898db7f00a54c49771d1de1281a
ldh1103 <ldh517525@gmail.com>
2023-04-04 09:59:20 +0900

    23-04-04

--- PHP/Training/12_2_tng1_SQL.php
+++ PHP/Training/12_2_tng1_SQL.php
@@ -3,24 +3,30 @@
     // 우리가 작성한 DB Connect 함수(my_db_conn)를 이용해서 아래 데이터를 출력
 
     include_once("../Example/12_2_ex2_fnc_db_conn.php");
-    $obj_conn = null;
-    my_db_conn( $obj_conn );
 
+
+    
     // 전체 월급의 평균
-    // $sql =
-    // " SELECT "
-    // ."    AVG(salary) "
-    // ." FROM " 
-    // ."    salaries "
-    // ." WHERE " 
-    // ."    to_date = :to_date ";
+    $sql =
+    " SELECT "
+    ."    AVG(salary) "
+    ." FROM " 
+    ."    salaries "
+    ." WHERE " 
+    ."    to_date = DATE(99990101) ";
 
-    // $to_date = "9999-01-01";
-    // $stmt = $obj_conn->prepare($sql);
-    // $stmt->bindParam(':to_date', $to_date);
+    // $obj_conn = null;
+    // my_db_conn( $obj_conn );
+    // $stmt = $obj_conn->prepare( $sql );
     // $stmt->execute();
     // $result = $stmt->fetch();
     // echo "AVG(salary) : $".$result["AVG(salary)"];
+    
+    $obj_conn = null;
+    my_db_conn( $obj_conn );
+    $stmt = $obj_conn->query( $sql );
+    $result = $stmt->fetch();
+    var_dump( $result );
 
     
     // 새로운 사원 정보를 employees 테이블에 등록
@@ -53,10 +59,13 @@
     //         , ":hire_date" => DATE(20200101)
     //     );
 
+    // $obj_conn = null;
+    // my_db_conn( $obj_conn );
     // $stmt = $obj_conn->prepare( $sql );
     // $result = $stmt->execute( $arr_prepare );
     // $obj_conn->commit();
     // var_dump( $result );
+
 
     // 위에서 입력한 사원의 이름을 "길동", 성은 "홍" 으로 변경
     // $sql =
@@ -75,10 +84,13 @@
     //         , ":emp_no" => 1
     //     );
 
+    // $obj_conn = null;
+    // my_db_conn( $obj_conn );
     // $stmt = $obj_conn->prepare( $sql );
     // $stmt->execute( $arr_prepare );
     // $obj_conn->commit();
     // var_dump( $result );
+
 
     // 위에서 등록한 사원을 삭제
     // $sql =
@@ -93,6 +105,8 @@
     //         ":emp_no" => 1
     //     );
 
+    // $obj_conn = null;
+    // my_db_conn( $obj_conn );
     // $stmt = $obj_conn->prepare( $sql );
     // $result = $stmt->execute( $arr_prepare );
     // $obj_conn->commit();
