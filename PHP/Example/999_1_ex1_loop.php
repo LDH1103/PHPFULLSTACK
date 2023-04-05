595b22766b80deaed34816cc764301b0e567b550
ldh1103 <ldh517525@gmail.com>
2023-04-05 11:34:16 +0900

    23-04-05

--- PHP/Example/999_1_ex1_loop.php
+++ PHP/Example/999_1_ex1_loop.php
@@ -1,4 +1,16 @@
 <?php
+/*
+파일명 :
+시스템명 :
+이력
+    v001 : new - 21(사번)
+    v002 : fnc_print_str 수정 - 21(사번)
+
+*/
+
+
+
+
 
     // while + if
     // $i = 0;
@@ -164,21 +176,83 @@
     // fnc_reference( $str );
     // echo $str;
 
-    function fnc_print_star( $param_int ) {
-        for ( $i = 0; $i < $param_int; $i++ ) { 
-            echo "*";
+    // 함수를 하나 만들고 여러번 호출해서 별찍기
+    // 수정 후 수정이력 남기기
+
+    // function fnc_print_star( $param_int ) { // v002 del
+    // function fnc_print_other( $param_str_print, $param_int ) { // v002 add
+    // // function fnc_print_other( $param_int, $param_str_print = "O" ) { // 넘겨받은 파라미터가 없다면 "O"가 출력됨
+    //     for ( $i = 0; $i < $param_int; $i++ ) { 
+    //         // echo "*"; // v002 del
+    //         echo $param_str_print; // v002 add
+    //     }
+    //     echo "\n";
+    // }
+
+    // // fnc_print_star( 1 ); // v002 del
+    // // fnc_print_star( 2 );
+    // // fnc_print_star( 3 );
+    // // fnc_print_star( 4 );
+    // // fnc_print_star( 5 ); 
+    // $str_print = "$"; // v002 add
+    // fnc_print_other( $str_print, 1 );
+    // fnc_print_other( $str_print, 2 );
+    // fnc_print_other( $str_print, 3 );
+    // fnc_print_other( $str_print, 4 );
+    // fnc_print_other( $str_print, 5 );
+
+
+    // function fnc_reference2( &$param_str ) { // &$param_str은 $str을 가리키고있음
+    //     echo "2번 : $param_str \n";
+    //     $param_str = "fnc_reference2에서 값 변경";
+    //     echo "3번 : $param_str \n";
+    // }
+    // $str = "aaa";
+    // echo "1번 : $str \n";
+    // fnc_reference2( $str );
+    // echo "4번 : $str \n";
+
+    // 1,2번 "aaa", 3,4번 "fnc_reference2에서 값 변경"
+
+
+    
+    // ---------- class ----------
+    class Food {
+        // 접근 제어 지시자
+        // public    : 어디서든(class 밖에서도) 접근 가능
+        // private   : 해당 class 내에서만 접근 가능
+        // protected : class 자기 자신과 상속 클래스 내에서만 접근 가능
+        
+        // 멤버 변수
+        private $str_name;
+        private $int_price;
+
+        // 메소드 = calss 안에 있는 함수
+        // setter
+        public function __construct( $param_name, $param_price ) {
+            $this->str_name = $param_name;
+            $this->int_price = $param_price;
         }
-        echo "\n";
-    }
 
-    fnc_print_star( 1 );
-    fnc_print_star( 2 );
-    fnc_print_star( 3 );
-    fnc_print_star( 4 );
-    fnc_print_star( 5 ); 
+        // getter
+        public function fnc_print_food() {
+            $str = $this->str_name." : ".$this->int_price."원\n";
+            echo $str;
+        }
 
-    for ($i = 0; $i <= 5; $i++) { 
-        fnc_print_star( $i ); 
+        public function fnc_change_price( $param_price ) {
+            $this->int_price = $param_price;
+        }
     }
+    
+    $obj_food = new Food( "탕수육", 10000 );
+    $obj_food->fnc_print_food();
+
+    // Food Class 멤버 변수 $int_price의 값을 12000으로 바꾸기
+
+    $obj_food->fnc_change_price( 12000 );
+    $obj_food->fnc_print_food();
+
+
 
 ?>
