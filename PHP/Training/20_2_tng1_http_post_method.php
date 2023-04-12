6b5321cc9e3130cdb0af6f63cf86b17c6ac7d584
ldh1103 <ldh517525@gmail.com>
2023-04-12 13:19:44 +0900

    23-04-12

--- PHP/Training/20_2_tng1_http_post_method.php
+++ PHP/Training/20_2_tng1_http_post_method.php
@@ -1,8 +1,10 @@
 <?php
-    // 1. Get Method로 사용자가 입력한 데이터를 HTTP Requset
+    // 1. Post Method로 사용자가 입력한 데이터를 HTTP Requset
     // 2. 입력한 데이터의 상세 내역
     //  2-1. key : id, pw, name, birth_date
-    // 3. echo를 이용해서 각각의 데이터를 출력
+    // 3. 유저가 입력하지 않은 데이터도 함께 전송
+    //  3-1. key : h_page, val : h1
+    // 4. echo를 이용해서 각각의 데이터를 출력
 ?>
 <!DOCTYPE html>
 <html lang="ko">
@@ -13,7 +15,7 @@
         <title>Document</title>
     </head>
     <body>
-        <form method="get" action="20_2_tng1_http_method.php">
+        <form method="post" action="20_2_tng1_http_post_method.php">
             <input type="text" name="id" placeholder="ID">
             <br>
             <input type="password" name="pw" placeholder="PW">
@@ -22,13 +24,14 @@
             <br>
             <input type="date" name="birth_date">
             <br>
+            <input type="hidden" name="h_page" value="h1">
             <button type="submit">Submit</button>
         </form>
     </body>
 </html>
 <?php
     echo "<br>";
-    $a = $_GET;
+    $a = $_POST;
     foreach ($a as $key => $val) {
         echo $key." : ".$val."<br>";
     }
