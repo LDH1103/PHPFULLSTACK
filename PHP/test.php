78b3bb53ea251441d96d8c748715d5316190af2d
ldh1103 <ldh517525@gmail.com>
2023-06-19 17:49:52 +0900

    23-06-19

--- PHP/test.php
+++ PHP/test.php
@@ -1,5 +1,14 @@
 <?php
 
-for ($i = 'A'; $i < 'Z'; $i++) { 
-    echo $i. "\n"; 
+// for ($i = 'A'; $i < 'Z'; $i++) { 
+//     echo $i. "\n"; 
+// }
+
+$arr = [];
+for($j = 101; $j <= 105; $j++) {
+    for($i = 0; $i < 5; $i++) {
+        $arr[] = ['plane_name' => 'A'.$j, 'total_seat_num' => 96 , 'line_no' => $i];
+    }
 }
+
+var_dump($arr);
