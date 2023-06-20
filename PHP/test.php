fa61c3365f5fefd471f69ed607dc34ead0ae4f7f
LDH1103 <ldh517525@gmail.com>
2023-06-20 20:12:36 +0900

    23-06-20

--- PHP/test.php
+++ PHP/test.php
@@ -4,11 +4,12 @@
 //     echo $i. "\n"; 
 // }
 
-$arr = [];
-for($j = 101; $j <= 105; $j++) {
-    for($i = 0; $i < 5; $i++) {
-        $arr[] = ['plane_name' => 'A'.$j, 'total_seat_num' => 96 , 'line_no' => $i];
-    }
-}
+// $arr = [];
+// for($j = 101; $j <= 105; $j++) {
+//     for($i = 0; $i < 5; $i++) {
+//         $arr[] = ['plane_name' => 'A'.$j, 'total_seat_num' => 96 , 'line_no' => $i];
+//     }
+// }
 
-var_dump($arr);
+// var_dump($arr);
+?>
