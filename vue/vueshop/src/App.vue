e9b397280e8862af752aad42cc50206844191965
LDH1103 <ldh517525@gmail.com>
2023-07-06 17:29:57 +0900

    23-07-06

--- vue/vueshop/src/App.vue
+++ vue/vueshop/src/App.vue
@@ -1,16 +1,40 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <div>
+    <h4>{{ product1 }}</h4>
+    <p>{{ price1 }}원</p>
+  </div>
+  <div>
+    <h4 :style="styleR">{{ product2 }}</h4>
+    <p>{{ price2 }}원</p>
+  </div> -->
+  <div v-for="item in products" :key="item">
+    <!-- 값만 -->
+    <!-- <h4 v-for="name in products" :key="name">{{ name }}</h4>  -->
+    <!-- 인덱스 -->
+    <!-- <h4 v-for="(name, i) in products" :key="name">{{ i }}</h4> -->
+    <h4>{{ item.name }}</h4>
+    <p>{{ item.price }}원</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() { // 데이터 바인딩
+    return {
+      products : [
+        {name : '티셔츠', price : '3800'}
+        ,{name : '바지', price : '5000'}
+        ,{name : '점퍼', price : '10000'}
+      ]
+      // ,product1 : '양말'
+      // ,price1 : '3800'
+      // ,product2 : '바지'
+      // ,price2 : '5000'
+      // ,styleR : 'color : red'
+    }
+  },
 }
 </script>
 
