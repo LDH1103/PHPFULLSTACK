07f454142acdfa36cc0e3f2fa63b408f8ddf2be7
LDH1103 <ldh517525@gmail.com>
2023-08-06 23:07:44 +0900

    23-08-06

--- PHP/coding_test.php
+++ PHP/coding_test.php
@@ -1,35 +1,35 @@
 <?php
 
-$arr = [1, 2, 3, 4, 5, 6];
-$arr_cnt = count($arr);
-$result_arr = []; 
+// $arr = [1, 2, 3, 4, 5, 6];
+// $arr_cnt = count($arr);
+// $result_arr = []; 
 
-for ($i = 0; $i < $arr_cnt; $i++) {
-    for ($j = $i + 1; $j < $arr_cnt; $j++) {
-        if ($arr[$i] + $arr[$j] === 7) {
-            $result_arr[] = $arr[$i] . ' + ' . $arr[$j];
-        }
-    }
-}
+// for ($i = 0; $i < $arr_cnt; $i++) {
+//     for ($j = $i + 1; $j < $arr_cnt; $j++) {
+//         if ($arr[$i] + $arr[$j] === 7) {
+//             $result_arr[] = $arr[$i] . ' + ' . $arr[$j];
+//         }
+//     }
+// }
 
-foreach ($result_arr as $val) {
-    echo $val . " = 7 \n";
-}
+// foreach ($result_arr as $val) {
+//     echo $val . " = 7 \n";
+// }
 
 // ------------------------------------------
 
-// function find_odd($int_1, $int_2) {
-//     $result = '';
+function find_odd($int_1, $int_2) {
+    $result = '';
 
-//     for ($i = $int_1; $i < $int_2; $i++) {
-//         if ($i % 2 !== 0) {
-//             $result .= $i . ' ';
-//         }
-//     }
+    for ($i = $int_1; $i < $int_2; $i++) {
+        if ($i % 2 !== 0) {
+            $result .= $i . ' ';
+        }
+    }
 
-//     return $result;
-// }
+    return $result;
+}
 
-// echo find_odd(2, 30);
+echo find_odd(2, 5);
 
 ?>
